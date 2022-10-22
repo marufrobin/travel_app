ce595971a85d14bea1e3798a36462a4d6cc3b535
Maruf Robin <maruf.robin@northsouth.edu>
2022-10-22 19:46:24 +0600

    homePage and basic design done with placeHolder.

---
+++ lib/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Travel Blog",
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            Expanded(flex: 2, child: Placeholder()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Most Popular",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Text(
+                    "View All",
+                    style: TextStyle(fontSize: 20, color: Colors.orange),
+                  ),
+                )
+              ],
+            ),
+            Expanded(flex: 1, child: Placeholder()),
+          ],
+        ),
+      ),
+    );
+  }
+}
